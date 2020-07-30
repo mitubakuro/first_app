1c97c724555759a822ff40e807c4ce5e61cb3584
mitubakuro <nakano1113mituba@gmail.com>
2020-07-30 14:29:40 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+
+  def index
+    @post = Post.find(1)
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.create(content: params[:content])
+  end
+
+
+end
